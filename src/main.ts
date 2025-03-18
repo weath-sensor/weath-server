a764900cceb25c58f05c7451ccf6a65b5a9406f5
Kamil-Janik <yaneczq1@gmail.com>
2025-03-18 22:16:05 +0100

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS for your frontend domain
+  app.enableCors({
+    origin: 'http://localhost:5173',  // Allow requests from your frontend (change as needed)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept',  // Allowed headers
+  });
+
+  await app.listen(3000);  // Port where your server runs
 }
 bootstrap();
