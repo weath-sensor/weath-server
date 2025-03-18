bb5fcc3a9f797aa8a878c456d9d0d89e50072d39
Kamil-Janik <yaneczq1@gmail.com>
2025-03-18 22:58:03 +0100

    ..

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for your frontend domain
+  // Enable CORS for your frontend domain (public IP of your VPS)
   app.enableCors({
-    origin: 'http://localhost:5173',  // Allow requests from your frontend (change as needed)
+    origin: 'http://51.222.111.230:5173',  // Replace with the public IP and port where your frontend is running
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Allowed HTTP methods
     allowedHeaders: 'Content-Type, Accept',  // Allowed headers
   });
 
-  await app.listen(3000, '0.0.0.0');  // Port where your server runs
+  await app.listen(3000, '0.0.0.0');  // Port where your API server runs
 }
 bootstrap();
