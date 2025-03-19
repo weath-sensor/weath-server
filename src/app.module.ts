64b924eb9ac1e80cf4f60bb7a27a86eac2222c40
Kamil-Janik <yaneczq1@gmail.com>
2025-03-19 09:11:29 +0100

    ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { LdrDataModule } from './ldr-data/ldr-data.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',  // Fallback to localhost if DB_HOST is undefined
-      port: parseInt(process.env.DB_PORT || '3306'),  // Fallback to 3306 if DB_PORT is undefined
-      username: process.env.DB_USERNAME || 'root',  // Default user
-      password: process.env.DB_PASSWORD || 'password',  // Default password
-      database: process.env.DB_DATABASE || 'weather_db',  // Default database
+      host: process.env.DB_HOST || 'localhost', 
+      port: parseInt(process.env.DB_PORT || '3306'),  
+      username: process.env.DB_USERNAME || 'root', 
+      password: process.env.DB_PASSWORD || 'password',  
+      database: process.env.DB_DATABASE || 'weather_db', 
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
