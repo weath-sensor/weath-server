b510d0732bb987a58c39d874f3b1db603596e5be
Kamil-Janik <yaneczq1@gmail.com>
2025-03-25 20:34:53 +0100

    update .env

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://51.222.111.230:5173',
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
